5ae45cf55813e5f4c5c7d43076c6a470fb0b08f1
lucky favour <luckyfavour1219@gmail.com>
2024-07-06 04:20:55 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Destination from './components/Destination'
+import Search from './components/Search';
+import Select from './components/select';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
       <Hero />
       <Destination />
       <Search />
+      <Select />
+      <Carousel />
+      <Footer />
     </div>
   );
 }
